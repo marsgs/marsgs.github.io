6cc578ea5c6ec0c078cb0b22d0e4a0ef0ac82959
Marisol Garcia <mgarci38@uncc.edu>
2020-08-19 17:14:36 -0400

    Updates

---
+++ precache-manifest.ce8ac5be7b4c1062eca87e26d4f1dc19.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d588aa3f0b3f3eca40a92f531505b53e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2eaadd0ef036d233ac27",
+    "url": "/static/css/main.4cee933f.chunk.css"
+  },
+  {
+    "revision": "e5c46c02c3920cf911c0",
+    "url": "/static/js/2.7db83802.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7db83802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eaadd0ef036d233ac27",
+    "url": "/static/js/main.89ae8341.chunk.js"
+  },
+  {
+    "revision": "93036b75017fd2fa9481",
+    "url": "/static/js/runtime-main.8f8e043f.js"
+  },
+  {
+    "revision": "a619f4c4ca89587ca812a0c20de92a38",
+    "url": "/static/media/Marisol-Garcia-Resume.a619f4c4.pdf"
+  },
+  {
+    "revision": "0567b854a692dc43b7b90aa2e364526c",
+    "url": "/static/media/blue-whale.0567b854.jpg"
+  },
+  {
+    "revision": "554299f546ea42696a11a98d2b1200fc",
+    "url": "/static/media/boat.554299f5.png"
+  },
+  {
+    "revision": "0760905649fc59835e556391fd301ba6",
+    "url": "/static/media/cases.07609056.gif"
+  },
+  {
+    "revision": "40e2a5dffa9ef0644270d57fc01c4a9a",
+    "url": "/static/media/chicks.40e2a5df.png"
+  },
+  {
+    "revision": "f6a2bad8ec077d67dfef303890efbc4b",
+    "url": "/static/media/fish-hair.f6a2bad8.png"
+  },
+  {
+    "revision": "d635681ddf6c41bdb52aaf713aba0248",
+    "url": "/static/media/map.d635681d.png"
+  },
+  {
+    "revision": "fd78c22ac6067188ad055007da73c52e",
+    "url": "/static/media/meal.fd78c22a.gif"
+  },
+  {
+    "revision": "db65c32bbdb5ea1415b8d11a36e8d72e",
+    "url": "/static/media/neon-girl.db65c32b.png"
+  },
+  {
+    "revision": "16bb1b2e030e1e06de2b92d41fd54f1a",
+    "url": "/static/media/octopus.16bb1b2e.jpeg"
+  },
+  {
+    "revision": "d47a40a7620f020c8c38e68a6f358fa2",
+    "url": "/static/media/prof.d47a40a7.png"
+  },
+  {
+    "revision": "5c2bcc4e7064423e217f6cf302f50421",
+    "url": "/static/media/qup.5c2bcc4e.png"
+  }
+]);
